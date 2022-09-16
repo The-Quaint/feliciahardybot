7872aaba3d62fc160868ae27de8aac503e465e92
The Quaint <87853928+The-Quaint@users.noreply.github.com>
2022-09-16 22:38:39 +0530

    Update __init__.py

--- wbb/__init__.py
+++ wbb/__init__.py
@@ -143,7 +143,7 @@ if USERBOT_ID not in SUDOERS:
 
 log.info("Initializing Telegraph client")
 telegraph = Telegraph()
-r = telegraph.create_account(short_name='KB5017328')
+r = telegraph.create_account(short_name='opweretgbncsfdter')
 auth_url = r["auth_url"]
 
 async def eor(msg: Message, **kwargs):
