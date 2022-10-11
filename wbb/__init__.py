b491f4a77f387023ff937609e5e2fce9aed1f4fd
The Quaint <87853928+The-Quaint@users.noreply.github.com>
2022-10-11 19:29:09 +0530

    Update __init__.py

--- wbb/__init__.py
+++ wbb/__init__.py
@@ -32,7 +32,6 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from pyromod import listen
 from Python_ARQ import ARQ
-from telegraph import Telegraph
 
 is_config = path.exists("config.py")
 
@@ -141,9 +140,6 @@ USERBOT_DC_ID = y.dc_id
 if USERBOT_ID not in SUDOERS:
     SUDOERS.add(USERBOT_ID)
 
-log.info("Initializing Telegraph client")
-telegraph = Telegraph()
-telegraph.create_account(short_name=BOT_USERNAME)
 
 
 async def eor(msg: Message, **kwargs):
