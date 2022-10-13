e13b3b75fad225c25d4d34069eb591c71e154563
The Quaint <87853928+The-Quaint@users.noreply.github.com>
2022-10-13 23:03:55 +0530

    Create rules.py

---
+++ wbb/modules/rules.py
@@ -0,0 +1,33 @@
+
+from wbb import app 
+from wbb import app2 
+from pyrogram import filters 
+...
+
+
+__MODULE__ = "Rules"
+__HELP__ = '''<b>Available Commands:</b>
+- /setrules (rules): saves the rules (also works with reply)
+- /rules: Shows the rules of chat if any!
+- /resetrules: Resets group's rules'''
+
+
+@app.on_message(filters.command("setrules") & ~filters.edited & ~filters.private)
+@adminsOnly("can_restrict_members")
+async def set_rules(_, message: Message):
+    chat_id = chat["chat_id"]
+
+    # FIXME: documents are allow to saved (why?), check for args if no 'reply_to_message'
+    note = await get_parsed_note_list(message, allow_reply_message=True, split_args=-1)
+    note["chat_id"] = chat_id
+
+    if (
+        await db.rules.replace_one({"chat_id": chat_id}, note, upsert=True)
+    ).modified_count > 0:
+        text = strings["updated"]
+    else:
+        text = strings["saved"]
+
+    await message.reply(text % chat["chat_title"])
+
+# Many useful functions are in, wbb/utils/, wbb, and wbb/core/
