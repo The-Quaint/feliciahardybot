3a946efc00dcaef2cb19bf806e5ff4e38c69e48c
The Quaint <87853928+The-Quaint@users.noreply.github.com>
2024-10-28 22:31:16 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,48 +1,25 @@
-from os import environ
+import os
 
 from dotenv import load_dotenv
 
-load_dotenv("config.env")
+load_dotenv(
+    "config.env" if os.path.isfile("config.env") else "sample_config.env"
+)
 
-HEROKU = bool(
-    environ.get("DYNO")
-)  # NOTE Make it false if you're not deploying on heroku or docker.
-
-if HEROKU:
-
-    BOT_TOKEN = environ.get("BOT_TOKEN", None)
-    API_ID = int(environ.get("API_ID", 6))
-    API_HASH = environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
-    SESSION_STRING = environ.get("SESSION_STRING", None)
-    USERBOT_PREFIX = environ.get("USERBOT_PREFIX", ".")
-    SUDO_USERS_ID = [int(x) for x in environ.get("SUDO_USERS_ID", "").split()]
-    LOG_GROUP_ID = int(environ.get("LOG_GROUP_ID", None))
-    GBAN_LOG_GROUP_ID = int(environ.get("GBAN_LOG_GROUP_ID", None))
-    MESSAGE_DUMP_CHAT = int(environ.get("MESSAGE_DUMP_CHAT", None))
-    WELCOME_DELAY_KICK_SEC = int(environ.get("WELCOME_DELAY_KICK_SEC", None))
-    MONGO_URL = environ.get("MONGO_URL", None)
-    ARQ_API_URL = environ.get("ARQ_API_URL", None)
-    ARQ_API_KEY = environ.get("ARQ_API_KEY", None)
-    LOG_MENTIONS = bool(int(environ.get("LOG_MENTIONS", None)))
-    RSS_DELAY = int(environ.get("RSS_DELAY", None))
-    PM_PERMIT = bool(int(environ.get("PM_PERMIT", None)))
-else:
-    BOT_TOKEN = "467677575:YZfaakjwd545dfg-N6JStihhuw5gQeZHntc"
-    API_ID = 123456
-    API_HASH = "dfxcgs5s12hdcxfgdfz"
-    USERBOT_PREFIX = "."
-    PHONE_NUMBER = "+916969696969"  # Need for Userbot
-    SUDO_USERS_ID = [
-        4543744343,
-        543214651351,
-    ]  # Sudo users have full access to everything, don't trust anyone
-    LOG_GROUP_ID = -100125431255
-    GBAN_LOG_GROUP_ID = -100125431255
-    MESSAGE_DUMP_CHAT = -1001181696437
-    WELCOME_DELAY_KICK_SEC = 300
-    MONGO_URL = "mongodb+srv://username:password@cluster0.ksiis.mongodb.net/YourDataBaseName?retryWrites=true&w=majority"
-    ARQ_API_KEY = "Get this from @ARQRobot"
-    ARQ_API_URL = "https://arq.hamker.in"
-    LOG_MENTIONS = True
-    RSS_DELAY = 300  # In seconds
-    PM_PERMIT = True
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+API_ID = int(os.environ.get("API_ID"))
+SESSION_STRING = os.environ.get("SESSION_STRING", "")
+API_HASH = os.environ.get("API_HASH")
+USERBOT_PREFIX = os.environ.get("USERBOT_PREFIX", "\\")
+PHONE_NUMBER = os.environ.get("PHONE_NUMBER")
+SUDO_USERS_ID = list(map(int, os.environ.get("SUDO_USERS_ID", "").split()))
+LOG_GROUP_ID = int(os.environ.get("LOG_GROUP_ID"))
+GBAN_LOG_GROUP_ID = int(os.environ.get("GBAN_LOG_GROUP_ID"))
+MESSAGE_DUMP_CHAT = int(os.environ.get("MESSAGE_DUMP_CHAT"))
+WELCOME_DELAY_KICK_SEC = int(os.environ.get("WELCOME_DELAY_KICK_SEC", 600))
+MONGO_URL = os.environ.get("MONGO_URL")
+ARQ_API_KEY = os.environ.get("ARQ_API_KEY")
+ARQ_API_URL = os.environ.get("ARQ_API_URL", "https://arq.hamker.dev")
+LOG_MENTIONS = os.environ.get("LOG_MENTIONS", "True").lower() in ["true", "1"]
+RSS_DELAY = int(os.environ.get("RSS_DELAY", 300))
+PM_PERMIT = os.environ.get("PM_PERMIT", "True").lower() in ["true", "1"]
